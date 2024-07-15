081b228626fbd956586617fa0f763104dcf3a724
MatheusVLanger <matheuslanger123@outlook.com>
2024-07-15 16:26:30 -0300

    Inicio do projeto de versionamento de codigo

---
+++ mod5/primeiroProjeto/src/br/com/matheus/PrimeiraClasse.java
@@ -0,0 +1,8 @@
+package br.com.matheus;
+
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        String nome= "Matheus";
+        System.out.println("Hello "+ nome);
+    }
+}
