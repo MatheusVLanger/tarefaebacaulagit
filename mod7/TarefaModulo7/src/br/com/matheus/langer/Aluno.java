940d077defbab0a291c8e182944a9265d9181407
MatheusVLanger <matheuslanger123@outlook.com>
2024-07-22 14:11:23 -0300

    Exercicioa modulo 7

---
+++ mod7/TarefaModulo7/src/br/com/matheus/langer/Aluno.java
@@ -0,0 +1,52 @@
+package br.com.matheus.langer;
+
+public class Aluno {
+
+    //Esse e um cometario
+
+    private String nome;
+
+    private String endereco;
+
+    private int numMatricula;
+
+    private String cpf;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getNumMatricula() {
+        return numMatricula;
+    }
+
+    public void setNumMatricula(int numMatricula) {
+        this.numMatricula = numMatricula;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double calcularMedia(double nota1, double nota2){
+
+        return (nota1+nota2)/2;
+    }
+}
